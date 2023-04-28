09d068389a5eae30afe2bfdaa5944e6fe0b8103e
JonteSkola <jonathan.stove@elev.ga.lbs.se>
2023-04-28 12:19:50 +0200

    Added food

--- Snek/Assets/Scripts/Movement.cs
+++ Snek/Assets/Scripts/Movement.cs
@@ -4,6 +4,33 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Vector2 direction = Vector2.right;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            direction = Vector2.up;
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            direction = Vector2.left;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            direction = Vector2.down;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            direction = Vector2.right;
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        this.transform.position = new Vector3(Mathf.Round(this.transform.position.x) + direction.x, Mathf.Round(this.transform.position.y) + direction.y, 0.0f);
+    }
+
+    /*
     public float speed;
 
     private Vector3 direction = Vector3.right;
@@ -29,11 +56,17 @@ public class Movement : MonoBehaviour
 
         transform.position += direction * speed * Time.deltaTime;
     }
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.gameObject.name.Contains("Wall"))
         {
             Destroy(gameObject);
         }
+        if (collision.transform.gameObject.name.Contains("Food"))
+        {
+
+        }
     }
+    */
 }
